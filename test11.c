b2f20eaea068b2fac5b755bbc7680f2388f3c85c
Rohini Mhaske <rohinimhaske04@gmail.com>
2021-05-27 18:18:22 +0530

    first commit

---
+++ test11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	 char alph,rev;                                                              
+    printf("Enter an alphabet  ");                                             
+    scanf("%c", &alph);                                                         
+                                                                                
+    if(islower(alph))                                                           
+      rev = toupper(alph);                                                      
+    else                                                                        
+      rev = tolower(alph);                                                      
+    printf("Reverse case of %c is : %c\n", alph, rev);                        
+                                                                                
+}                                   
+
